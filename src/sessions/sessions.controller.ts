806655a3b2d42a200d33f2664d7fefe6da51202e
alexanderhernandez17 <alexander.hdez.2001@gmail.com>
2024-06-20 13:48:37 -0500

    endpoints fixed

--- src/sessions/sessions.controller.ts
+++ src/sessions/sessions.controller.ts
@@ -1,11 +1,41 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, ParseIntPipe, DefaultValuePipe } from '@nestjs/common';
 import { SessionsService } from './sessions.service';
 import { CreateSessionDto } from './dto/create-session.dto';
 import { UpdateSessionDto } from './dto/update-session.dto';
+import { Session } from './entities/session.entity';
+import { Workspace } from 'src/workspaces/entities/workspace.entity';
 
 @Controller('sessions')
 export class SessionsController {
   constructor(private readonly sessionsService: SessionsService) {}
+
+  @Get('occupied')
+  getSessionsOrderedByOccupied(
+    @Query('page', ParseIntPipe) page = 1,
+    @Query('limit', ParseIntPipe) limit = 10,
+    @Query('order') order: 'ASC' | 'DESC' = 'ASC'
+  ): Promise<Session[]> {
+    return this.sessionsService.getSessionsOrderedByOccupied(page, limit, order);
+  }
+
+  @Get('available')
+  getSessionsOrderedByAvailable(
+    @Query('page', ParseIntPipe) page = 1,
+    @Query('limit', ParseIntPipe) limit = 10,
+    @Query('order') order: 'ASC' | 'DESC' = 'ASC'
+  ): Promise<Session[]> {
+    return this.sessionsService.getSessionsOrderedByAvailable(page, limit, order);
+  }
+
+
+  @Get(':sessionId/workspaces')
+  async getWorkspacesBySession(
+    @Param('sessionId', ParseIntPipe) sessionId: number,
+    @Query('page', ParseIntPipe) page: number = 1,
+    @Query('limit', ParseIntPipe) limit: number = 10,
+  ): Promise<{ workspaces: Workspace[], total: number }> {
+    return this.sessionsService.getWorkspacesBySession(sessionId, page, limit);
+  }
 
   @Post()
   create(@Body() createSessionDto: CreateSessionDto) {
--- src/workspaces/workspaces.service.ts
+++ src/workspaces/workspaces.service.ts
@@ -1,26 +1,62 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Workspace } from './entities/workspace.entity';
 import { CreateWorkspaceDto } from './dto/create-workspace.dto';
 import { UpdateWorkspaceDto } from './dto/update-workspace.dto';
 
 @Injectable()
 export class WorkspacesService {
-  create(createWorkspaceDto: CreateWorkspaceDto) {
+  constructor(
+    @InjectRepository(Workspace)
+    private workspaceRepository: Repository<Workspace>,
+  ) {}
+
+  async getAvailableWorkspaces(sessionId: number, page: number, limit: number, order: 'ASC' | 'DESC'): Promise<Workspace[]> {
+    const query = this.workspaceRepository.createQueryBuilder('workspace')
+      .leftJoin('workspace.bookings', 'booking')
+      .leftJoin('workspace.session', 'session')
+      .leftJoin('booking.user', 'user')
+      .where('session.id = :sessionId', { sessionId })
+      .andWhere('user.id IS NULL')
+      .skip((page - 1) * limit)
+      .take(limit)
+      .orderBy('workspace.id', order);
+
+    return query.getMany();
+  }
+
+  async getOccupiedWorkspaces(sessionId: number, page: number, limit: number, order: 'ASC' | 'DESC'): Promise<Workspace[]> {
+    const query = this.workspaceRepository.createQueryBuilder('workspace')
+      .leftJoin('workspace.bookings', 'booking')
+      .leftJoin('workspace.session', 'session')
+      .leftJoin('booking.user', 'user')
+      .where('session.id = :sessionId', { sessionId })
+      .andWhere('user.id IS NOT NULL')
+      .skip((page - 1) * limit)
+      .take(limit)
+      .orderBy('workspace.id', order);
+
+    return query.getMany();
+  }
+
+  async create(createWorkspaceDto: CreateWorkspaceDto) {
     return 'This action adds a new workspace';
   }
 
-  findAll() {
+  async findAll() {
     return `This action returns all workspaces`;
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
     return `This action returns a #${id} workspace`;
   }
 
-  update(id: number, updateWorkspaceDto: UpdateWorkspaceDto) {
+  async update(id: number, updateWorkspaceDto: UpdateWorkspaceDto) {
     return `This action updates a #${id} workspace`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} workspace`;
   }
 }
